12b0a522eabd2f27514c34c54602ad0eb8e93fd4
1click-tech <1clickdistributors.tech@gmail.com>
2025-11-24 14:51:27 +0530

    some small changes

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,16 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata = {
   title: "1Clickdistributors",
